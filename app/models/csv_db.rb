429ef9bea894afaffe62fc51e4372da03cb1f6f6
dilpreet92 <dilpreet@vinsol.com>
2016-07-05 00:03:42 +0530

    save changes

--- app/models/csv_db.rb
+++ app/models/csv_db.rb
@@ -2,8 +2,9 @@ require 'csv'
 class CsvDb
   class << self
     def convert_save(target_model, csv_data, &block)
-      csv_file = csv_data.read
-      CSV.parse(csv_file, :headers => true, header_converters: :symbol ) do |row|
+      file = File.open(File.join(Rails.root, 'public', 'csv_files', csv_data), 'r')
+      csv_file = file.read
+      CSV.parse(csv_file, :headers => true, header_converters: :symbol, col_sep: '&&&&&' ) do |row|
         data = row.to_hash
         if data.present?
           if (block_given?)
@@ -13,6 +14,7 @@ class CsvDb
            end
          end
       end
+      File.delete(File.join(Rails.root, 'public', 'csv_files', csv_data))
     end
   end
 end
--- lib/active_admin_importable/dsl.rb
+++ lib/active_admin_importable/dsl.rb
@@ -10,7 +10,10 @@ module ActiveAdminImportable
       end
 
       collection_action :import_csv, :method => :post do
-        CsvDb.convert_save(active_admin_config.resource_class, params[:dump][:file], &block)
+        file = params[:dump][:file].read
+        filename = params[:dump][:file].original_filename
+        File.open(File.join(Rails.root, 'public', 'csv_files', filename), 'wb') { |f| f.write file }
+        CsvDb.delay.convert_save(active_admin_config.resource_class, filename, &block)
         redirect_to :action => :index, :notice => "#{active_admin_config.resource_name.to_s} imported successfully!"
       end
     end
