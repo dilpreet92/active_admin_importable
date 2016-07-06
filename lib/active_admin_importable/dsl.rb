90f8b1861d5cadeed92b6bd1688e90dd50e46e16
dilpreet92 <dilpreet@vinsol.com>
2016-07-06 21:21:36 +0530

    add s3 object

--- lib/active_admin_importable/dsl.rb
+++ lib/active_admin_importable/dsl.rb
@@ -10,13 +10,11 @@ module ActiveAdminImportable
       end
 
       collection_action :import_csv, :method => :post do
-        file = params[:dump][:file].read
+        s3 = AWS::S3.new
         filename = params[:dump][:file].original_filename
-        obj = S3_BUCKET.objects[filename]
-        obj.write(
-          file: params[:dump][:file],
-          acl: :public_read
-        )
+        key = File.basename(filename)
+        s3.buckets[Rails.application.secrets.bucket].acl = :public_read
+        obj = s3.buckets[Rails.application.secrets.bucket].objects[key].write(:file => params[:dump][:file], acl:'public-read')
         CsvDb.delay.convert_save(active_admin_config.resource_class, obj.public_url.to_s, &block)
         redirect_to :action => :index, :notice => "#{active_admin_config.resource_name.to_s} imported successfully!"
       end
